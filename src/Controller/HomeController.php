601a33b934f4ef3dcc256d8aee3f850608238626
RudyStark <89338357+RudyStark@users.noreply.github.com>
2023-04-06 12:20:19 +0200

    New update User

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,8 @@
 
 namespace App\Controller;
 
-use App\Repository\IngredientRepository;
-use App\Repository\MenuRepository;
-use App\Repository\PlatRepository;
 use App\Repository\RestaurantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
